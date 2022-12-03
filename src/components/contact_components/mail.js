a032aafd0a39d1f0e37f4e75d2a4a046440af6b4
Konrad Strauss <konrad.strauss.97@gmail.com>
2022-12-03 10:49:20 +0100

    kra

--- src/components/contact_components/mail.js
+++ src/components/contact_components/mail.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 import ContactData from "./dane";
-
+import Message from "./message";
 
 export default function Mail(){
+    const data={}
+    const[submit,send]=useState(false)
 
+    function collectData(object){
+        data={...data, object}
+    }
     return(
         <>
-        <ContactData id='mailConatct'></ContactData>
+        <ContactData id='mailContact' submit={submit} value={collectData} />
+        <Message title='Treść wiadomości' submit={submit} value={collectData}/>
         </>
     )
 }
---
+++ src/components/contact_components/submit_button.js
@@ -0,0 +1,13 @@
+
+export default SubmitButton()
+{
+
+    function submitHandler(
+
+    )
+
+    return(
+        <input type='submit'  onSubmit={submitHandler}>Wyślij</input>
+    )
+
+}
