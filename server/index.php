c45d2081dd0e37867dc1a312798d6990a740ccf6
FlightlessParrot <113798246+FlightlessParrot@users.noreply.github.com>
2022-12-15 01:01:12 +0100

    111

--- server/index.php
+++ server/index.php
@@ -25,20 +25,49 @@ class mail
     }
     function sent($others)
     {
-        $msg=$this->name.' '.$this->tel.' '.$this->message.' '.$others;
-        mail('konrad.strauss.97@gmail.com', "Mail od " . $this->name, $msg, 'frome: ' . $this->mail);
+        $msg=$this->name.' '.$this->tel.' <br> '.$this->message.' <br> '.$others;
+        mail('konrad.strauss.97@gmail.com', "Mail od ".$this->name, $msg, 'frome: '.$this->mail);
     }
 }
 
 class offer extends mail
 {
-    private $sklep;
-    private $money;
-    private $panel;
-    private $forum;
+    private $mess;
+    private $oth;
     function __construct()
-    {
-        
+    {    
+        $this->oth='';
+        $this->mess=array(
+            array('ilość podstron ', $_POST['page'] ),
+            array('sklep internetowy ', $_POST['sklep'] ),
+            array('płatności internetowe ', $_POST['money']),
+            array('panel administratora ', $_POST['panel']),
+            array('forum ', $_POST['forum']),
+            array('galeria ', $_POST['galeria']),
+            array('formularz: ', $_POST['formularz'])
+        );
+     $this->prepareInfo();
+
     }
+    private function prepareInfo()
+    {
+        foreach($this->mess as $value)
+        {
+            $this->oath.=$value[0].$value[1].'<br>';
+        }
+    }
+  
 }
+if(isset($_POST['type']))
+{
+if($_POST['type']==='mail')
+{
+$mailMan=new mail;
+$mailMan->sent('');
+}
+else{
+    $mailMan=new offer;
+    $mailMan->sent($mailMan->oath);
+}}
+echo 'I am working'
 ?>
